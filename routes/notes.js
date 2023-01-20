30af6aad3cd9fe2be7e490b252af5389881b4ad7
Zachary-Rossman <zrossman@yahoo.com>
2023-01-19 18:01:08 -0600

    added code for notes; app still not functional

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,24 @@
-const notes = require('express').Router();
+const notes = require("express").Router();
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const { v4: uuidv4 } = require("uuid");
 
-// GET Route for retrieving all the tips
-notes.get('/', (req, res) => {
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-});
+// GET Route for retrieving all the notes
+notes.get("/", (req, res) => {    
+    console.log(req.body);
 
-// POST Route for a new UX/UI tip
-// tips.post('/', (req, res) => {
-//   console.log(req.body);
+    const { title, text } = req.body;
 
-module.exports = notes;
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
+
+        readAndAppend(newNote, "./db/db.json");
+        res.json('Note added successfully');
+    } else {
+        res.error("Error in adding note");
+    }});
+
+    module.exports = notes;
